b1104bcd6ff29328d3cf386f81a3b130e9736043
Hamza Khan <hamza.khan951hk@gmail.com>
2024-09-12 19:21:52 +0500

    switch.js

---
+++ switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+// General syntax to write a switch
+const month = 3;
+switch (month) {
+  case 1: //We can also use string in comparision
+    console.log("January");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+
+  default:
+    console.log("Default case match");
+    break;
+}
